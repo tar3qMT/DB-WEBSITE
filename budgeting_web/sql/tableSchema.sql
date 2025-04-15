199a506e91d88091e8bc3eb994166ecf082d3a74
Rahma Seid <84355185+rahmaseid@users.noreply.github.com>
2025-04-15 13:41:09 -0500

    Add files via upload

--- budgeting_web/sql/tableSchema.sql
+++ budgeting_web/sql/tableSchema.sql
@@ -6,46 +6,39 @@ CREATE TABLE users (
     password VARCHAR(100) NOT NULL,
     date_joined TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
 -- Income Table: track income sources for financial analysis
 CREATE TABLE Income (
     income_id INT PRIMARY KEY AUTO_INCREMENT,
     user_id INT,
     income_source VARCHAR(100) NOT NULL,
-    income_amount DECIMAL(10,2) NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+    income_amount DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
-
 -- Categories Table: group categories into food, electric, transportation etc
 CREATE TABLE Categories (
     category_id INT PRIMARY KEY AUTO_INCREMENT,
     category_name VARCHAR(100) NOT NULL
 );
-
 -- SavingsGoals Table: tracks user's saving goals
-CREATE TABLE SavingsGoals (
+CREATE TABLE SavingGoals (
     goal_id INT PRIMARY KEY AUTO_INCREMENT,
     user_id INT NOT NULL,
     goal_name VARCHAR(100) NOT NULL,
     target_amount DECIMAL(10, 2) NOT NULL,
     current_amount DECIMAL(10, 2) DEFAULT 0,
-    category_id INT,
-    FOREIGN KEY (user_id) REFERENCES Users(user_id),
-    FOREIGN KEY (category_id) REFERENCES Categories(category_id)
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
-
 -- Transactions Table: tracks user's spending activity
 CREATE TABLE Transactions (
-    transaction_id INT PRIMARY KEY AUTO_INCREMENT,
+    transaction_id INT PRIMARY KEY auto_increment,
     user_id INT,
     transaction_amount DECIMAL(10, 2) NOT NULL,
     category_id INT,
     transaction_date DATE NOT NULL,
     transaction_location VARCHAR(100),
-    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
     FOREIGN KEY (category_id) REFERENCES Categories(category_id)
 );
-
 -- Bills Table: stores expenses like rent, utilities, etc.
 CREATE TABLE Bills (
     bill_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -54,17 +47,21 @@ CREATE TABLE Bills (
     bill_amount DECIMAL(10, 2) NOT NULL,
     due_date DATE NOT NULL,
     payment_date DATE,
-    paid_status BOOLEAN DEFAULT FALSE,
-    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+    payment_status BOOLEAN DEFAULT FALSE,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
-
 -- Report Table: generates monthly report to let user know about purchases and budget status
 CREATE TABLE Report (
     report_id INT PRIMARY KEY AUTO_INCREMENT,
     user_id INT NOT NULL,
-    report_month DATE NOT NULL,
-    total_income DECIMAL(10,2) NOT NULL,
-    total_expenses DECIMAL(10,2) NOT NULL,
+    report_month CHAR(100) NOT NULL,
+    total_income DECIMAL(10, 2) NOT NULL DEFAULT 0.00,
+    total_transactions DECIMAL(10, 2) NOT NULL DEFAULT 0.00,
     budget_status ENUM('under budget', 'on budget', 'over budget') NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+    UNIQUE(user_id, report_month),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
+INSERT INTO Categories (category_id, category_name)
+VALUES (1, 'General'),
+    (2, 'Shopping'),
+    (3, 'Dining');
