93d23ae27ef9dc851e86ddc5d46d79c5514d2988
Rahma Seid <84355185+rahmaseid@users.noreply.github.com>
2025-04-15 11:18:55 -0500

    Delete back_end directory

--- back_end/auth/session_check.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-require_once '../back_end/session_check.php';
-if (!isset($_SESSION['user_id'])) {
-    header("Location: ../front_end/signin.html");
-    exit;
-}
--- back_end/auth/session_info.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-header('Content-Type: application/json');
-
-echo json_encode([
-    'username' => $_SESSION['username'] ?? 'Guest'
-]);
